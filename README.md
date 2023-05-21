ff5cf6757ad7c33d825fc9618279a3aa3277b0af
Thomas Bertoluzzi <bertoluzzit2@gmail.com>
2023-05-21 17:54:31 -0300

    Commit 1, Ejercicio 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Taller de Lenguajes I
+## TP 3
+### Bertoluzzi Thomas
---
+++ practico6/Program.cs
@@ -0,0 +1,36 @@
+﻿// Introducción 
+
+/*
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
+*/
+
+// Ejercicio 1
+
+
+int num = 0;
+string aux;
+int resto;
+Console.WriteLine("Ingrese un numero: ");
+aux = Console.ReadLine();
+bool resultado = int.TryParse(aux, out num);
+if (resultado == true)
+{
+    aux = "";
+    while (num > 0)
+    {
+        resto = num % 10;
+        num = num / 10;
+        aux = aux + String.Concat(resto);   // El numero es vuelto a convertir en String para mostrar el 0 al comienzo
+    }
+    Console.WriteLine("El numero invertido es: " + aux);
+}
+else
+{
+    Console.WriteLine("No se ingresó un numero.");
+}
